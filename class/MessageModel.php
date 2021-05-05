bf742db7ba1173adc666dc9c71d0cbcc75df8d59
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-05-05 09:59:45 -0300

    Model criado

--- class/MessageModel.php
+++ class/MessageModel.php
@@ -2,13 +2,17 @@
 require 'Connection.php';
 class MessageModel
 {
-    
+    private $con;
+    function __construct(){
+        $this->con = Connection::connect();
+    }
+
+    public function static all(){
+        $smtp = $this->con->prepare('SELECT * FROM CUR_MENSAGEM');
+        $smtp->execute();
+        return $smtp->fetchAll(PDO::FETCH_OBJ);
+    }
 }
-$con = Connection::connect();
-$smtp = $con->prepare('SELECT * FROM CUR_MENSAGEM');
-$smtp->execute();
-$linhas = $smtp->fetchAll(PDO::FETCH_OBJ);
-foreach($linhas as $linha)
-{
-    echo $linha->TELEFONE;
-}
+
+
+
